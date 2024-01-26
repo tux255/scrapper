4f6bfbb3cbb812bcf81fc8e8cf998607b74358d5
Andrii Haranin <andrii.haranin@teladochealth.com>
2024-01-26 23:48:11 +0100

    Meta scrapping added

--- app/services/fields_parser_service.rb
+++ app/services/fields_parser_service.rb
@@ -3,14 +3,24 @@ require 'nokogiri'
 class FieldsParserService
   def self.perform(html, fields)
     html_doc = Nokogiri::HTML(html)
-    
     fields_found = {}
 
-    fields.each do |k,v| 
-      found = html_doc.css(v)
+    if fields['meta'].present?
+      fields_found['meta'] = {}
 
+      fields['meta'].each { |name|
+        fields_found['meta'][name] = html_doc.css("meta[name='#{name}']").last['content']
+      }
+    end
+
+    fields.each do |k, v|
+      next if k == 'meta'
+
+      found = html_doc.css(v)
       fields_found[k] = found.text
     end
+
+    binding.pry
 
     fields_found
   end
--- app/services/html_parser_service.rb
+++ app/services/html_parser_service.rb
@@ -4,7 +4,7 @@ class HtmlParserService
   def self.perform(url)
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
-    
+
     response.body
   end
 end

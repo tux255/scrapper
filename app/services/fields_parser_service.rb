c778e69c1ce576e2e88cf1eff786400342b1454a
Andrii Haranin <andrii.haranin@teladochealth.com>
2024-01-28 21:54:38 +0100

    Added error handlin

--- app/services/fields_parser_service.rb
+++ app/services/fields_parser_service.rb
@@ -5,21 +5,26 @@ class FieldsParserService
     html_doc = Nokogiri::HTML(html)
     fields_found = {}
 
-    fields_found['meta'] = fields['meta'].each_with_object({}) do |name, result|
-      meta_tag = html_doc.css("meta[name='#{name}']").last
-      result[name] = meta_tag['content'] if meta_tag
-    end
+    begin
+      fields_found['meta'] = fields['meta'].each_with_object({}) do |name, result|
+        meta_tag = html_doc.css("meta[name='#{name}']").last
+        result[name] = meta_tag['content'] if meta_tag
+      end
 
-    fields.each do |k, v|
-      next if k == 'meta'
+      fields.each do |k, v|
+        next if k == 'meta'
 
-      found = html_doc.css(v)
+        found = html_doc.css(v)
 
-      fields_found[k] = if found.count > 1
-                          found.map { |f| f.try(:text) }
-                        else
-                          found.try(:text)
-                        end
+        fields_found[k] = if found.count > 1
+                            found.map { |f| f.try(:text) }
+                          else
+                            found.try(:text)
+                          end
+      end
+    rescue StandardError => e
+      Rails.logger.error("Error parsing HTML: #{e.message}")
+      fields_found = {}
     end
 
     fields_found
--- app/services/html_parser_service.rb
+++ app/services/html_parser_service.rb
@@ -2,9 +2,15 @@ require 'net/http'
 
 class HtmlParserService
   def self.perform(url)
-    uri = URI.parse(url)
-    response = Net::HTTP.get_response(uri)
-
-    response.body
+    begin
+      uri = URI.parse(url)
+      response = Net::HTTP.get_response(uri)
+  
+      response.body
+    rescue StandardError => e
+      Rails.logger.error("Error fetching HTML from #{url}: #{e.message}")
+      nil
+    end
   end
 end
+

f2260a30cf179a7cefe43493d928cf1585bd68d7
Andrii Haranin <andrii.haranin@teladochealth.com>
2024-01-29 16:33:45 +0100

    Error handlind added

--- app/services/html_parser_service.rb
+++ app/services/html_parser_service.rb
@@ -5,12 +5,15 @@ class HtmlParserService
     begin
       uri = URI.parse(url)
       response = Net::HTTP.get_response(uri)
-  
-      response.body
+
+      if response.is_a?(Net::HTTPSuccess)
+        return response.body
+      else
+        raise StandardError, "HTTP request to #{url} failed with code #{response.code}: #{response.message}"
+      end
     rescue StandardError => e
       Rails.logger.error("Error fetching HTML from #{url}: #{e.message}")
       nil
     end
   end
 end
-
